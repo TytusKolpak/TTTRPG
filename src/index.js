5113b58c55b7cec7d22870de3b6f99ef405ca8d0
TytusKolpak <101522301+TytusKolpak@users.noreply.github.com>
2023-07-15 21:09:24 +0200

    You can play the eating game now.

--- src/index.js
+++ src/index.js
@@ -1,15 +1,66 @@
-function processValue() {
-    // Clear previous state of cells displayed
-    $(".grid-container").empty();
+var columns, rows, player, amountOfCells;
+const board = $(".grid-container");
 
-    // Number of times to add the div elements
-    var amountOfCells = parseInt(document.getElementById("numericInput").value);
+window.onload = (event) => {
+    console.log("page is fully loaded");
+    init();
+};
+
+window.onresize = () => {
+    adjustSizes();
+}
+
+function init() {
+    //better x by y lol
+    amountOfCells = 49
+    createBoard(amountOfCells);
+    addPlayer();
+    adjustSizes();
+}
+
+function adjustSizes() {
+    // Function for adjusting element sizes based on the window size
+    var width = board.width();
+    var height = board.height();
+
+    // Cells
+    const margin = 2 * columns;
+    var cellWidth = (width - margin) / columns;
+    var cellHeight = (height - margin) / rows;
+    $(".rc").css("width", cellWidth)
+    $(".rc").css("height", cellHeight)
+    // Otherwise the cell will stretch to the img size 512px
+}
+
+function createBoard(size) {
+    player = null
+    amountOfCells;
+
+    // If there was no function call with an argument then use the user provided one
+    if (size) {
+        amountOfCells = size;
+        console.log("Default amount of cells.");
+    } else {
+        // Number of times to add the div elements
+        amountOfCells = parseInt(document.getElementById("numericInput").value);
+        if (amountOfCells > 900) {
+            alert("Ok, that's too much.")
+            return;
+        } else {
+            console.log("Good amount of cells provided.");
+        }
+    }
+
+    // Clear previous state of cells displayed
+    board.empty();
 
     // Creating these elements
     for (var i = 1; i <= amountOfCells; i++) {
-        var div = $("<div class='rc'></div>");
-        div.text(i);
-        $(".grid-container").append(div);
+        var cell = $("<div class='rc'></div>");
+        var cellNumberDiv = $("<div class='cellNumber'></div>");
+        cellNumberDiv.text(i);
+        cell.append(cellNumberDiv);
+        board.append(cell);
     }
 
     // Now we want to arrange them into a shape that's as close to a square as possible. We do that by winding square root of the number of elements and then round it up so that we will always get more rows/columns than necessary. We also assume that there will be more columns than rows.
@@ -17,7 +68,18 @@ function processValue() {
     // By getting the rounded up value X we can generate a css property of grid-template-columns with value as auto repeated X times.
 
     var autos = "";
-    var columns = Math.ceil(Math.sqrt(amountOfCells));
+    columns = Math.ceil(Math.sqrt(amountOfCells));
+
+    // Determine how many rows are there. Let's assume we have 16 cells then its 4x4. 5x4 for 17,18,19,20 and for 21 we have 5x5 again. This goes up to 25, and for 26 we have 6x5 up to 30, at 31 we have 6x6.
+    // It so happens that Math.sqrt(amountOfCells) passes n.5 at the very moment when we add a row.
+    var remains = Math.sqrt(amountOfCells) % 1;
+    if (remains > 0.5 | remains == 0) {
+        // console.log("even");
+        rows = columns;
+    } else {
+        // console.log("odd");
+        rows = columns - 1;
+    }
 
     // Generate property's value of correct length 
     for (let i = 0; i < columns; i++) {
@@ -25,5 +87,10 @@ function processValue() {
     }
 
     // Apply styles
-    $(".grid-container").css("grid-template-columns", autos);
+    board.css("grid-template-columns", autos);
+
+    // Create a prey in the last cell
+    var lastCell = board.children().eq(-1)
+    var farmer = $("<img>").attr("src", "../img/farmer.png");
+    $(lastCell).append(farmer);
 }
---
+++ src/player.js
@@ -0,0 +1,75 @@
+function Player(position, speed, power, range, name, title, imageUrl) {
+    this.position = position;
+    this.speed = speed;
+    this.power = power;
+    this.range = range;
+    this.name = name;
+    this.title = title;
+    this.imageUrl = imageUrl;
+}
+
+function addPlayer() {
+    player = new Player(1, 10, 50, 20, "Dracula", "Vampire Lord", "../img/dracula.png");
+    displayPlayer(player)
+}
+
+function displayPlayer(player) {
+    $("."+player.name).remove(); // remove any previous instances
+    var playerDiv = $("<div class='player'></div>");
+    playerDiv.addClass(player.name);
+    var image = $("<img>").attr("src", player.imageUrl);
+    playerDiv.append(image);
+    var cell = board.children().eq(player.position)
+    cell.append(playerDiv);
+    adjustSizes();
+}
+
+$("html").keydown((event) => {
+    if (player) {
+        switch (event.key) {
+            case "ArrowRight":
+                if ((player.position + 1) % columns == 0) {
+                    console.log("right border");
+                } else {
+                    player.position += 1;
+                }
+                break;
+
+            case "ArrowLeft":
+                if (player.position % columns == 0) {
+                    console.log("left border");
+                } else {
+                    player.position -= 1;
+                }
+                break;
+
+            case "ArrowUp":
+                if (player.position < columns) {
+                    console.log("top border");
+                } else {
+                    player.position -= columns;
+                }
+                break;
+
+            case "ArrowDown":
+                if (player.position > (amountOfCells - 1 - columns)) {
+                    console.log("bottom border")
+                } else {
+                    player.position += columns;
+                }
+                break;
+
+            default:
+                console.log("No binding.");
+                break;
+        }
+
+        if (player.position == amountOfCells-1) {
+            console.log("You eat :E!");
+
+            // remove contents of last child of the board (so last cell)
+            board.children().eq(-1).children().remove();
+        }
+        displayPlayer(player)
+    }
+});
